a49d6e12113fb912bbad3845beefafe492eeb92d
Sujal Singh <email.sujalsingh@gmail.com>
2022-03-24 19:52:14 +0530

    NEW: installer

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Aakarshit1999/Yoga-Pose-Dectection
+cd Yoga-Pose-Dectection/
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
