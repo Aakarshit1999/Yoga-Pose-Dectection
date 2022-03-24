136e11b443a884c5993d1f9954665d47e120287a
Aakarshit Pandey <aakarshit6143@gmail.com>
2022-03-24 20:14:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ git clone https://github.com/Aakarshit1999/Yoga-Pose-Dectection
 cd Yoga-Pose-Dectection/
 pip install virtualenv
 python -m virtualenv venv
-source venv/bin/activate
+source venv/Scripts/activate
 pip install -r requirements.txt
 exec bash
